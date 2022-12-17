84970b6ee406f821321d8d1c58bf6bdfac0995d2
AvocadoCoding <116954089+AvocadoCoding@users.noreply.github.com>
2022-12-17 19:59:18 +0000

    function to color row backgrounds according to time added

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 currentDayEl = $('#currentDay');
+textAreasClass = $('.description')
 
 // handle displaying the time
 function displayTime() {
@@ -6,5 +7,28 @@ function displayTime() {
     currentDayEl.text(rightNow);
   }
 
-
   setInterval(displayTime, 1000);
+
+  console.log(('#hour-11').childNodes)
+
+  // A for loop to identify all of the rows in the html file
+
+  for(var i=9; i<18; i++){
+    var identifier = '#hour-' + i;
+    var identifierEl= $(identifier);
+    console.log(identifier);
+    console.log(identifierEl);
+    
+    // an if function is added to check for each row if the current hour is 
+    // smaller than, greater than or equal to the row placeholder time, and to
+    // then color the row text area accordingly
+
+    if(i < (moment().format('HH'))){
+      identifierEl.children("textarea").css({"background-color":"lightgrey"});
+    } else if(i > (moment().format('HH'))){
+      identifierEl.children("textarea").css({"background-color":"red"});
+    } else{
+      identifierEl.children("textarea").css({"background-color":"green"});
+    }
+
+  }
